43a38866e361c2fa2e793805389b6dd028a7dd20
Oleksiuk Bohdan <lordapos@gmail.com>
2021-03-17 12:38:51 +0200

    add products logic redux

--- frontend/src/redux/reducers/productReducer.js
+++ frontend/src/redux/reducers/productReducer.js
@@ -1,4 +1,8 @@
 import {
+  PRODUCT_CREATE_FAIL,
+  PRODUCT_CREATE_REQUEST, PRODUCT_CREATE_RESET, PRODUCT_CREATE_SUCCESS,
+  PRODUCT_DELETE_FAIL,
+  PRODUCT_DELETE_REQUEST, PRODUCT_DELETE_SUCCESS,
   PRODUCT_DETAILS_CLEAN,
   PRODUCT_DETAILS_FAIL,
   PRODUCT_DETAILS_REQUEST,
@@ -6,7 +10,7 @@ import {
   PRODUCT_LIST_FAIL,
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
-  PRODUCT_LIST_VIEW,
+  PRODUCT_LIST_VIEW, PRODUCT_UPDATE_FAIL, PRODUCT_UPDATE_REQUEST, PRODUCT_UPDATE_RESET, PRODUCT_UPDATE_SUCCESS,
 } from '../actions/actionTypes'
 
 export const productViewReducer = (state = { productsView: 'big-cards' }, action) => {
@@ -31,7 +35,7 @@ export const productListReducer = (state = { products: [] }, action) => {
   }
 }
 
-export const productDetailsReducer = (state = { product: {} }, action ) => {
+export const productDetailsReducer = (state = { product: {} }, action) => {
   switch (action.type) {
     case PRODUCT_DETAILS_REQUEST:
       return { loading: true, ...state }
@@ -40,7 +44,50 @@ export const productDetailsReducer = (state = { product: {} }, action ) => {
     case PRODUCT_DETAILS_FAIL:
       return { loading: false, error: action.payload }
     case PRODUCT_DETAILS_CLEAN:
-      return { loading: true, product: {}}
+      return { loading: true, product: {} }
+    default:
+      return state
+  }
+}
+
+export const productDeleteReducer = (state = {}, action) => {
+  switch (action.type) {
+    case PRODUCT_DELETE_REQUEST:
+      return { loading: true }
+    case PRODUCT_DELETE_SUCCESS:
+      return { loading: false, success: true }
+    case PRODUCT_DELETE_FAIL:
+      return { loading: false, error: action.payload }
+    default:
+      return state
+  }
+}
+
+export const productUpdateReducer = (state = { product: {} }, action) => {
+  switch (action.type) {
+    case PRODUCT_UPDATE_REQUEST:
+      return { loading: true }
+    case PRODUCT_UPDATE_SUCCESS:
+      return { loading: false, success: true, product: action.payload }
+    case PRODUCT_UPDATE_FAIL:
+      return { loading: false, error: action.payload }
+    case PRODUCT_UPDATE_RESET:
+      return { product: {} }
+    default:
+      return state
+  }
+}
+
+export const productCreateReducer = (state = {}, action) => {
+  switch (action.type) {
+    case PRODUCT_CREATE_REQUEST:
+      return { loading: true }
+    case PRODUCT_CREATE_SUCCESS:
+      return { loading: false, success: true, product: action.payload }
+    case PRODUCT_CREATE_FAIL:
+      return { loading: false, error: action.payload }
+    case PRODUCT_CREATE_RESET:
+      return {}
     default:
       return state
   }
