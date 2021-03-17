14c86dd921c72e291d15c56a69c368d091bb6a00
Oleksiuk Bohdan <lordapos@gmail.com>
2021-03-17 12:09:44 +0200

    update Cart logic

--- backend/graphql/public_api/resolver/productResolver.js
+++ backend/graphql/public_api/resolver/productResolver.js
@@ -1,4 +1,6 @@
 const Product = require('../../../models/Product')
+const Sequelize = require('sequelize')
+const Op = Sequelize.Op
 
 module.exports = {
   async getProducts () {
@@ -52,6 +54,33 @@ module.exports = {
       return await Product.findAll(args)
     } catch (e) {
       throw new Error('Fetch products is not available')
+    }
+  },
+
+  async updateCart ({ items }) {
+    const ids = []
+    items.forEach(item => {
+      ids.push(item.id)
+    })
+    try {
+      const products = await Product.findAll({
+        where: {
+          id: {
+            [Op.or]: [ids],
+          },
+        },
+      })
+
+      const newProduct = []
+      products.forEach(item => {
+        const current = items.find(x => x.id === item.id)
+        item.dataValues.qty = current.qty
+        newProduct.push(item.dataValues)
+      })
+
+      return newProduct
+    } catch (e) {
+      throw new Error('Update cart is not available')
     }
   },
 }
