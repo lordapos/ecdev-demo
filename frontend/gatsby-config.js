24a0deb81f445289aa1281280ef390cdea128cac
Oleksiuk Bohdan <lordapos@gmail.com>
2021-03-18 13:59:49 +0200

    fix sitemap

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -9,7 +9,12 @@ module.exports = {
     "gatsby-plugin-sass",
     "gatsby-plugin-image",
     "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        exclude: [`/admin/*`, `/order`, `/admin`],
+      },
+    },
     {
       resolve: "gatsby-plugin-manifest",
       options: {
