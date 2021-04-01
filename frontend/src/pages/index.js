c152b1ff312b8c761cb0eb6add6a61c53c8b9aa7
devOleksandr <dev.oleksandrOliinyk@gmail.com>
2021-04-01 10:17:45 +0300

    add IncredibleShots component

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -2,12 +2,14 @@ import * as React from 'react'
 import Layout from '../components/Layout/Layout'
 import SEO from '../components/Seo'
 import CamerasReview from '../components/CamerasReview/CamerasReview'
+import IncredibleShots from '../components/IncredibleShots/IncredibleShots'
 
 const IndexPage = () => {
   return (
     <Layout>
       <SEO title='Find the best products'/>
       <CamerasReview/>
+      <IncredibleShots/>
     </Layout>
   )
 }
