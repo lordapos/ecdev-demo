db81e330a2985ce511d0c8763520e023604f4f15
Oleksiuk Bohdan <lordapos@gmail.com>
2021-04-19 13:00:32 +0300

    edits

--- frontend/src/components/Layout/Layout.js
+++ frontend/src/components/Layout/Layout.js
@@ -1,16 +1,25 @@
 import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import Footer from '../Footer/Footer'
 import Header from '../Header/Header'
 import MobileMenu from '../MobileMenu/MobileMenu'
 import { toggleMenu } from '../../redux/actions/appAction'
 import ReviewPopup from '../ReviewPopup/ReviewPopup'
 
-const Layout = ({ children, history, youtubeEmbed, productId }) => {
+const Layout = ({ children, history, productId }) => {
+  const mobMenu = useSelector((state) => state.app.visibleMobileMenu)
   const dispatch = useDispatch()
   useEffect(() => {
     dispatch(toggleMenu(false))
   }, [dispatch, history])
+
+  useEffect(() => {
+    if (mobMenu) {
+      document.body.classList.add("no-scroll")
+    } else {
+      document.body.classList.remove("no-scroll")
+    }
+  }, [dispatch, mobMenu])
   return (
     <>
       <Header/>
